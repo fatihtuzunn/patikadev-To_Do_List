a857e40bdebd57b0c534c322b0f2acb262332176
fatihtuzunn <fatihtuzun11@gmail.com>
2022-02-10 20:26:17 +0300

    last update

--- script.js
+++ script.js
@@ -1,62 +1,114 @@
 /* const toast = document.querySelector(`.toast`);
       toast. */
-      /* const b1 = document.getElementById(`b1`);
- const toast = document.querySelector(`.toast`);
+/* const b1 = document.getElementById(`b1`);
+const toast = document.querySelector(`.toast`);
  
- b1.click(()=>{$('.toast').toast('show')}); */
- $(document).ready(function () {
+b1.click(()=>{$('.toast').toast('show')}); */
+$(document).ready(function () {
 
     $("#showToast").click(function () {
-      $('#liveToast1').toast('show');
+        $('#liveToast1').toast('show');
     });
 
-  });
+});
 
-  const buton = document.getElementById("liveToastBtn");
-  const task = document.getElementById("task");
-  const ul = document.querySelector("ul");
-  
-  //burası çok önemli !!!!!
-  var taskArr = localStorage.getItem('task') ? JSON.parse(localStorage.getItem('task')) : [];
+const buton = document.getElementById("liveToastBtn");
+const task = document.getElementById("task");
+const ul = document.querySelector("ul");
 
 
-  
-  /* buton.addEventListener("click", ()=>{
-    console.log(task.value);
+
+//burası çok önemli !!!!!
+var taskArr = localStorage.getItem('task') ? JSON.parse(localStorage.getItem('task')) : [];
 
 
-  }) */
 
-  function makeLi(write) {
+/* buton.addEventListener("click", ()=>{
+  console.log(task.value);
+
+
+}) */
+
+function makeLi(write, idNum) {
     var li = document.createElement("li");
     ul.appendChild(li);
     var text = document.createTextNode(write);
     li.appendChild(text);
-  }
 
-  function newElement() {
+    /*   span elementi oluşturulacak
+       li elementinin altına yerleştrilecek
+       span elementinin içerisine "close" classı eklenecek 
+       
+       li.appendChild(span);
+   
+       */
+    var spann = document.createElement("span");
+    spann.classList.add(`close`);
+    spann.appendChild(document.createTextNode(`x`));
+    li.appendChild(spann);
+    spann.id = idNum;
+
+
+}
+
+
+
+
+
+function newElement() {
     taskArr.push(task.value);
     console.log(taskArr);
     localStorage.setItem("task", JSON.stringify(taskArr));
 
-      var taskArrLast= JSON.parse(localStorage.getItem("task"));
-
-     for (let i = 0; i < taskArrLast.length; i++) {
-       var element = taskArrLast[i];
-       
-     }
-     makeLi(element); 
-  }
-
-  addEventListener("load", () => {
-    var taskArrLast = JSON.parse(localStorage.getItem("task"));
+    let taskArrLast = JSON.parse(localStorage.getItem("task"));
 
     for (let i = 0; i < taskArrLast.length; i++) {
-      var element = taskArrLast[i];
-makeLi(element);
+        var element = taskArrLast[i];
+        
+
     }
+
+    makeLi(element);
+}
+
+
+
+
+
+
+
+
+addEventListener("load", () => {
+    let taskArrLast = JSON.parse(localStorage.getItem("task"));
+
+    for (let i = 0; i < taskArrLast.length; i++) {
+        var element = taskArrLast[i];
+        makeLi(element, i);
+
+
+    }
+    close();
+
+})
+
+ function close() {
+    let taskArrLast = JSON.parse(localStorage.getItem("task"));
+    /* let spanid = document.getElementById()
+    for (let i = 0; i < taskArrLast.length; i++) {
+        
+        const element = taskArrLast[i];
+        
+    } */
     
+    const spann = document.querySelectorAll("li")
+
+for (let i = 0 ; i < spann.length; i++) {
+    spann[i].addEventListener('click' , ()=>{
+        taskArr.splice(i, 1);
+        localStorage.setItem("task", JSON.stringify(taskArr));
+        location.reload();
 
 
-
-  })
+    } ) ; 
+ }
+}
